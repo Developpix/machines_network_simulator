807ba1831d9b845c61f620ec0292aa6062d963da
Developpix <developpix@gmail.com>
2018-04-06 15:24:26 +0200

    Create a class to create a bluetooth network.

---
+++ src/BluetoothNetwork.java
@@ -0,0 +1,30 @@
+package networks;
+
+import java.util.ArrayList;
+
+import cards.NetworkCard;
+
+/** A class to create a network to connect 2 network's cards.
+@author Developpix
+@version 0.1
+*/
+public class BluetoothNetwork extends Network {
+
+    /** Constructor for the BluetoothNetwork's class.
+    @param hisNumber a number for the bluetooth network.
+    */
+    public BluetoothNetwork(int hisNumber) {
+
+        super(hisNumber);
+
+    }
+
+    /** Method to get the maximum number of network's card connected.
+    @return the maximum number of network's card which can be connected.
+    */
+    public int getMaxNumberOfNetworkCard() {
+
+        return 2;
+
+    }
+}
